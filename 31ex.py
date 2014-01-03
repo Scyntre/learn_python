95dca183b93b54c6222bc34b3029f7e8df4bbc89
Troy Carter <troydc@gmail.com>
2014-01-03 12:30:29 -0600

    This is my fourth commit from server

---
+++ 31ex.py
@@ -0,0 +1,9 @@
+'''
+computing my budget for the month
+'''
+income = input("What is your income for the month?")
+print income
+expenses = input("What are your expenses for the month?")
+print expenses
+total = income - expenses 
+print "Your total spending money left for the month is: ", total
