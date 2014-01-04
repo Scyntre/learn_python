ae2556a603ac153dadcfc75fa9f0722b78dbb371
Troy Carter <troydc@gmail.com>
2014-01-03 20:58:10 -0600

    testing friday

---
+++ 5ex1.py
@@ -0,0 +1,9 @@
+#convert inches to centimeters
+x = raw_input("Input inches to be converted to centimeters:")
+cents = 2.54 * float(x)
+print "Centimeters:", cents
+
+#convert pounds to kilograms
+lb = raw_input("Input pounds to be converted to kilograms:")
+kilos = 2.2 * float(lb)
+print "Kilograms:", kilos
