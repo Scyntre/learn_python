8953cd37981373a74bba564d6148b9bd9ca8270f
Troy Carter <troydc@gmail.com>
2014-01-17 12:05:41 -0600

    working with ex17 about to move on

--- 17ex.py
+++ 17ex.py
@@ -4,10 +4,14 @@ from os.path import exists
 script, from_file, to_file = argv
 
 print "Copying from %s to %s" % (from_file, to_file)
-
-# we could do these two on one line too, how?
+'''
+we could do these two on one line too, how?
 in_file = open(from_file)
 indata = in_file.read()
+The folowing line requires that I kill in_file.close() at the end
+indata = open(from_file).read()
+'''
+in_file = open(from_file); indata = in_file.read()
 
 print "The input file is %d bytes long" % len(indata)
 
